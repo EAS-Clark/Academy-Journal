9a3dc2863ae1f2c9b0b71edac70979355a1203df
EAS-Clark <98745819+EAS-Clark@users.noreply.github.com>
2022-02-16 13:51:10 +0000

    Add files via upload

---
+++ Networking/Networking infrastructure scripts/Main_networking_script.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+#############
+#Clark brooks
+############
+# Setting server infrastructure
+
+
+
+
+
+
+
+
+
+
+
+https://www.linuxtechi.com/setup-bind-server-centos-8-rhel-8/
+
+https://www.tecmint.com/install-dhcp-server-client-on-centos-ubuntu/
+
+http://howto-madkour.blogspot.com/2013/08/linuxcentos-gateway-server.html?m=1
+
+
+
+
