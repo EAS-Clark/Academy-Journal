d00c1f8b188be190d1713f2b14e89b235a731143
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-02-22 11:50:23 +0000

    changed all brackets to echo

--- Networking/Networking_infrastructure_scripts/DHCP_Script.bash
+++ Networking/Networking_infrastructure_scripts/DHCP_Script.bash
@@ -5,19 +5,17 @@
 # Setting DHCP infrastructure
 
 
-# Argument 1 will be first part of the network address ($ipAddrees123)
-ipAddrees123="$1"
+# Argument 1 will be first part of the network address ($ipAddress123)
+ipAddress123="$1"
 
 # Argument 4 Domain name  (template)
 domainName="$2"
-
-
 
 yum install -y dhcp
 
 rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 
-{
+echo "
 
 	TYPE=Ethernet
 	PROXY_METHOD=none
@@ -32,13 +30,13 @@ rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 	UUID=3207c5eb-362e-4764-8ddd-847ab5c94e8e
 	DEVICE=ens160
 	ONBOOT=yes
-	IPADDR=$ipAddrees123.2
+	IPADDR=$ipAddress123.2
 	NETMASK:255.255.255.0
-	GATEWAY=$ipAddrees123.1
-	DNS=$ipAddrees123.3
+	GATEWAY=$ipAddress123.1
+	DNS=$ipAddress123.3
 	PREFIX=24
 
-}/etc/sysconfig/network-scripts/ifcfg-ens160
+" >> /etc/sysconfig/network-scripts/ifcfg-ens160
 
 firewall-cmd --zone=public --permanent --add-service=dhcp
 firewall-cmd --reload 
@@ -46,24 +44,24 @@ firewall-cmd --reload
 
 rm -f /etc/dhcp/dhcpd.conf
 
-{
+echo "
 	default-lease-time	86400; 
 	max-lease-time		172800;
 
 	option domain name "$domainName.com";
-	option doma in-name-servers $ipAddrees123.3;
+	option doma in-name-servers $ipAddress123.3;
 
 	ddns-update-style none;
 
 	authoritative;
 
-	subnet $ipAddrees123.0 netmask 255.255.255.0 {
-		range $ipAddrees123.10 $ipAddrees123.20; 
-		option broadcast-address $ipAddrees123.255;
-		option routers $ipAddrees123.1;
+	subnet $ipAddress123.0 netmask 255.255.255.0 {
+		range $ipAddress123.10 $ipAddress123.20; 
+		option broadcast-address $ipAddress123.255;
+		option routers $ipAddress123.1;
 	}
 
-}/etc/dhcp/dhcpd.conf
+" >> /etc/dhcp/dhcpd.conf
 
 
 
--- Networking/Networking_infrastructure_scripts/DNS_Script.bash
+++ Networking/Networking_infrastructure_scripts/DNS_Script.bash
@@ -4,10 +4,8 @@
 ############
 # Setting DNS infrastructure
 
-
-
-# Argument 1 will be first part of the network address ($ipAddrees123)
-ipAddrees123="$1"
+# Argument 1 will be first part of the network address ($ipAddress123)
+ipAddress123="$1"
 
 # Argument 2 will be last part of the network address (3)
 ipFourthOctave="3"
@@ -18,7 +16,6 @@ domainName="$2"
 
 dnf install -y bind bind-utils
 
-# 			^	is this how you yes for all?
 
 # test for error install ^
 
@@ -32,11 +29,9 @@ systemctl status named
 
 
 
-
-#
 rm -f /etc/named.conf
 
-{
+echo "
 	//
 	// named.conf
 	//
@@ -47,7 +42,7 @@ rm -f /etc/named.conf
 	//
 
 	options {
-		listen-on port 53 { $ipAddrees123.$ipFourthOctave; }; 
+		listen-on port 53 { $ipAddress123.$ipFourthOctave; }; 
 		listen-on-v6 port 53 { ::1; };
 		directory		"/var/named";
 		dump-file 		"/var/named/data/cache_dump.db";
@@ -55,7 +50,7 @@ rm -f /etc/named.conf
 		memstatistics-file "/var/named/data/named_mem stats.txt";
 		secroots-file "zuar/named/data/named.secroots";
 		recursing-file "/var/named/data/named .recursing"
-		allow-query (localhost: $ipAddrees123.0/24; };
+		allow-query (localhost: $ipAddress123.0/24; };
 		
 		/*
 			- If you are building an AUTHORITATIVE DNS server, do NOT enable recursion.
@@ -105,17 +100,17 @@ rm -f /etc/named.conf
 
 	//back zone
 
-	zone "$ipAddrees123.192.in-addr.arpa" IN {
+	zone "$ipAddress123.192.in-addr.arpa" IN {
 		type master;
 		file "$domainName.local.rev";
 		allow-update { none; };
 		allow-query { any; };
 	};
-} >> /etc/named.conf
+" >> /etc/named.conf
 
 
 rm -f /var/named/$domainName.local.db
-{
+echo "
 	$TTL 86400
 	@ IN SOA dns-primary.$domainName.local. admin.$domainName.local. (
 													2020011800 ;Serial
@@ -129,22 +124,22 @@ rm -f /var/named/$domainName.local.db
 	@ IN NS dns-primary.$domainName.local.
 
 	;IP Address for Name Server
-	dns-primary IN A $ipAddrees123.$ipFourthOctave
+	dns-primary IN A $ipAddress123.$ipFourthOctave
 
 	;A Record for the following Host name
 
-	Gateway   IN   A   $ipAddrees123.1
-	DHCP  IN   A   $ipAddrees123.2
-	DNS  IN   A   $ipAddrees123.$ipFourthOctave
-	app  IN   A   $ipAddrees123.10
+	Gateway   IN   A   $ipAddress123.1
+	DHCP  IN   A   $ipAddress123.2
+	DNS  IN   A   $ipAddress123.$ipFourthOctave
+	app  IN   A   $ipAddress123.10
 	
 	;CNAME Record
 	ftp  IN   CNAME www.$domainName.local.
-}/var/named/$domainName.local.db
+" >> /var/named/$domainName.local.db
 
 
 rm -f /var/named/$domainName.local.rev
-{
+echo "
 	$TTL 86400
 	@ IN SOA dns-primary.$domainName. admin.$domainName.local. (
 												2020011800 ;Serial
@@ -155,7 +150,7 @@ rm -f /var/named/$domainName.local.rev
 	)
 	;Name Server Information
 	@ IN NS dns-primary.$domainName.local.
-	dns-primary     IN      A       $ipAddrees123.$ipFourthOctave
+	dns-primary     IN      A       $ipAddress123.$ipFourthOctave
 
 	;Reverse lookup for Name Server
 	35 IN PTR dns-primary.$domainName.local.
@@ -163,7 +158,7 @@ rm -f /var/named/$domainName.local.rev
 	;PTR Record IP address to Hostname
 	50      IN      PTR     www.$domainName.local.
 
-}/var/named/$domainName.local.rev
+" >> /var/named/$domainName.local.rev
 
 
 chown named:named /var/named/$domainName.db
@@ -171,7 +166,7 @@ chown named:named /var/named/$domainName.rev
 
 named-checkconf
 named-checkzone $domainName.local /var/named/$domainName.db
-named-checkzone $ipAddrees123.$ipFourthOctave /var/named/$domainName.local.rev
+named-checkzone $ipAddress123.$ipFourthOctave /var/named/$domainName.local.rev
 
 
 systemctl restart named
@@ -181,7 +176,7 @@ firewall-cmd --reload
 
 rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 
-{
+echo "
 	TYPE=Ethernet
 	PROXY_METHOD=none
 	BROWSER_ONLY=no
@@ -196,9 +191,9 @@ rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 	UUID=32d7c5eb-362e-4764-8ddd-847ab5c94e8e
 	DEVICE=ens160
 	ONBOOT=yes
-	IPADDR=$ipAddrees123.$ipFourthOctave
+	IPADDR=$ipAddress123.$ipFourthOctave
 	NETMASK=255.255.255.0
-}/etc/sysconfig/network-scripts/ifcfg-ens160
+" >> /etc/sysconfig/network-scripts/ifcfg-ens160
 
 systemctl restart NetworkManager
 
