12b35a232c97fe87f0416aeebb1864916333371b
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-02 11:42:14 +0000

    Removed all indentation

--- Networking/Networking_infrastructure_scripts/DHCP_Script.bash
+++ Networking/Networking_infrastructure_scripts/DHCP_Script.bash
@@ -15,29 +15,25 @@ yum install -y dhcp
 
 rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 
-echo "
-
-	TYPE=Ethernet
-	PROXY_METHOD=none
-	BROWSER_ONLY=no
-	BOOTPROTO=staticpetpet
-	DEFROUTE=yes
-	IPV4_FAILURE_FATAL=no
-	IPV6INIT=yes
-	IPV6_AUTOCONF=yes
-	IPV6 DEFROUTE=yes
-	IPV6_FAILURE_FATAL=no
-	NAME=ens160
-	UUID=3207c5eb-362e-4764-8ddd-847ab5c94e8e
-	DEVICE=ens160
-	ONBOOT=yes
-	IPADDR=$ipAddress123.2
-	NETMASK:255.255.255.0
-	GATEWAY=$ipAddress123.1
-	DNS=$ipAddress123.3
-	PREFIX=24
-
-" >> /etc/sysconfig/network-scripts/ifcfg-ens160
+echo "TYPE=Ethernet
+PROXY_METHOD=none
+BROWSER_ONLY=no
+BOOTPROTO=static
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+IPV6INIT=yes
+IPV6_AUTOCONF=yes
+IPV6 DEFROUTE=yes
+IPV6_FAILURE_FATAL=no
+NAME=ens160
+UUID=3207c5eb-362e-4764-8ddd-847ab5c94e8e
+DEVICE=ens160
+ONBOOT=yes
+IPADDR=$ipAddress123.2
+NETMASK:255.255.255.0
+GATEWAY=$ipAddress123.1
+DNS=$ipAddress123.3
+PREFIX=24" >> /etc/sysconfig/network-scripts/ifcfg-ens160
 
 firewall-cmd --zone=public --permanent --add-service=dhcp
 firewall-cmd --reload 
@@ -45,24 +41,21 @@ firewall-cmd --reload
 
 rm -f /etc/dhcp/dhcpd.conf
 
-echo "
-	default-lease-time	86400; 
-	max-lease-time		172800;
+echo "default-lease-time	86400; 
+max-lease-time		172800;
 
-	option domain name "$domainName.com";
-	option doma in-name-servers $ipAddress123.3;
+option domain name "$domainName.com";
+option doma in-name-servers $ipAddress123.3;
 
-	ddns-update-style none;
+ddns-update-style none;
 
-	authoritative;
+authoritative;
 
-	subnet $ipAddress123.0 netmask 255.255.255.0 {
-		range $ipAddress123.10 $ipAddress123.20; 
-		option broadcast-address $ipAddress123.255;
-		option routers $ipAddress123.1;
-	}
-
-" >> /etc/dhcp/dhcpd.conf
+subnet $ipAddress123.0 netmask 255.255.255.0 {
+	range $ipAddress123.10 $ipAddress123.20; 
+	option broadcast-address $ipAddress123.255;
+	option routers $ipAddress123.1;
+}" >> /etc/dhcp/dhcpd.conf
 
 
 
--- Networking/Networking_infrastructure_scripts/DNS_Script.bash
+++ Networking/Networking_infrastructure_scripts/DNS_Script.bash
@@ -28,134 +28,106 @@ systemctl status named
 
 rm -f /etc/named.conf
 
-echo "
-	//
-	// named.conf
-	//
-	//Provided by Red Hat bind package to configure the ISC BIND named (8) DNS 
-	// server as a caching only nameserver (as a localhost DNS resolver only).
-	//
-	// See /usr/share/doc/bind#/sample/ for example named configuration files.
-	//
+echo "options {
+	listen-on port 53 { $ipAddress123.$ipFourthOctave; }; 
+	listen-on-v6 port 53 { ::1; };
+	directory		"/var/named";
+	dump-file 		"/var/named/data/cache_dump.db";
+	statistics-file "zuar/named/data/named stats.txt";
+	memstatistics-file "/var/named/data/named_mem stats.txt";
+	secroots-file "zuar/named/data/named.secroots";
+	recursing-file "/var/named/data/named .recursing"
+	allow-query (localhost: $ipAddress123.0/24; };
+	
+	recursion yes;
+	dnssec-enable yes;
+	dnssec-validation yes;
+		
+	managed-keys-directory "/var/named/dynamic";
+		
+	pid-file "/run/named/named.pid";
+	session-keyf ile "/run/named/session.key";
+		
+	/* https://fedoraproject.org/wiki/Changes/CryptoPolicy */
+	include "/etc/crypto-policies/back-ends/bind.config"
+};
 
-	options {
-		listen-on port 53 { $ipAddress123.$ipFourthOctave; }; 
-		listen-on-v6 port 53 { ::1; };
-		directory		"/var/named";
-		dump-file 		"/var/named/data/cache_dump.db";
-		statistics-file "zuar/named/data/named stats.txt";
-		memstatistics-file "/var/named/data/named_mem stats.txt";
-		secroots-file "zuar/named/data/named.secroots";
-		recursing-file "/var/named/data/named .recursing"
-		allow-query (localhost: $ipAddress123.0/24; };
-		
-		/*
-			- If you are building an AUTHORITATIVE DNS server, do NOT enable recursion.
-			- If you are building a RECURSIVE (caching) DNS server, you need to enable
-			  recursion. 
-			- If your recursive DNS server has a public IP address, you MUST enable access 
-			control to limit queries to your legitimate users. Failing to do so will
-			cause your server to become part of large scale DNS amplification
-			attacks. Implementing BCP38 within your network would greatly
-			reduce such attack surface
-		*/
-
-		recursion yes;
-		dnssec-enable yes;
-		dnssec-validation yes;
-		
-		managed-keys-directory "/var/named/dynamic";
-		
-
-		pid-file "/run/named/named.pid";
-		session-keyf ile "/run/named/session.key";
-		
-		/* https://fedoraproject.org/wiki/Changes/CryptoPolicy */
-		include "/etc/crypto-policies/back-ends/bind.config"
+logging {
+	channel default_debug {
+		file "data/named.run";
+		severity dynamic;
 	};
+};
 
-	logging {
-		channel default_debug {
-			file "data/named.run";
-			severity dynamic;
-		};
-	};
+zone "." IN {
+	type hint;
+	file "named.ca";
+};
 
-	zone "." IN {
-		type hint;
-		file "named.ca";
-	};
-
-	//foward zone
+//foward zone
 
 	zone "$domainName.local" IN {
-		type master;
-		file "$domainName.local.db";
-		allow-update { none; };
-		allow-query { any; };
-	};
+type master;
+	file "$domainName.local.db";
+	allow-update { none; };
+	allow-query { any; };
+};
 
-	//back zone
+//back zone
 
 	zone "$ipAddress123.192.in-addr.arpa" IN {
-		type master;
-		file "$domainName.local.rev";
-		allow-update { none; };
-		allow-query { any; };
-	};
-" >> /etc/named.conf
+	type master;
+	file "$domainName.local.rev";
+	allow-update { none; };
+	allow-query { any; };
+};" >> /etc/named.conf
 
 
 rm -f /var/named/$domainName.local.db
-echo "
-	$TTL 86400
-	@ IN SOA dns-primary.$domainName.local. admin.$domainName.local. (
-													2020011800 ;Serial
-													3600 ;Refresh
-													1800 ;Retry
-													604800 ;Expire
-													86400 ;Minimum TTL
-	)
-
-	;Name Server Information
-	@ IN NS dns-primary.$domainName.local.
-
-	;IP Address for Name Server
-	dns-primary IN A $ipAddress123.$ipFourthOctave
-
-	;A Record for the following Host name
-
-	Gateway   IN   A   $ipAddress123.1
-	DHCP  IN   A   $ipAddress123.2
-	DNS  IN   A   $ipAddress123.$ipFourthOctave
-	app  IN   A   $ipAddress123.10
-	
-	;CNAME Record
-	ftp  IN   CNAME www.$domainName.local.
-" >> /var/named/$domainName.local.db
-
-
-rm -f /var/named/$domainName.local.rev
-echo "
-	$TTL 86400
-	@ IN SOA dns-primary.$domainName. admin.$domainName.local. (
+echo "$TTL 86400
+@ IN SOA dns-primary.$domainName.local. admin.$domainName.local. (
 												2020011800 ;Serial
 												3600 ;Refresh
 												1800 ;Retry
 												604800 ;Expire
 												86400 ;Minimum TTL
-	)
-	;Name Server Information
-	@ IN NS dns-primary.$domainName.local.
-	dns-primary     IN      A       $ipAddress123.$ipFourthOctave
+)
 
-	;Reverse lookup for Name Server
-	35 IN PTR dns-primary.$domainName.local.
+;Name Server Information
+@ IN NS dns-primary.$domainName.local.
 
-	;PTR Record IP address to Hostname
-	50      IN      PTR     www.$domainName.local.
+;IP Address for Name Server
+dns-primary IN A $ipAddress123.$ipFourthOctave
 
-" >> /var/named/$domainName.local.rev
+;A Record for the following Host name
+
+Gateway   IN   A   $ipAddress123.1
+DHCP  IN   A   $ipAddress123.2
+DNS  IN   A   $ipAddress123.$ipFourthOctave
+app  IN   A   $ipAddress123.10
+	
+;CNAME Record
+ftp  IN   CNAME www.$domainName.local." >> /var/named/$domainName.local.db
+
+
+rm -f /var/named/$domainName.local.rev
+echo "$TTL 86400
+@ IN SOA dns-primary.$domainName. admin.$domainName.local. (
+											2020011800 ;Serial
+											3600 ;Refresh
+											1800 ;Retry
+											604800 ;Expire
+											86400 ;Minimum TTL
+)
+;Name Server Information
+@ IN NS dns-primary.$domainName.local.
+dns-primary     IN      A       $ipAddress123.$ipFourthOctave
+
+;Reverse lookup for Name Server
+35 IN PTR dns-primary.$domainName.local.
+
+;PTR Record IP address to Hostname
+50      IN      PTR     www.$domainName.local." >> /var/named/$domainName.local.rev
 
 
 chown named:named /var/named/$domainName.db
@@ -173,23 +145,22 @@ firewall-cmd --reload
 
 rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 
-echo "
-	TYPE=Ethernet
-	PROXY_METHOD=none
-	BROWSER_ONLY=no
-	BOOTPROTO=static
-	DEFROUTE=yes
-	IPV4_FAILURE_FATAL=no
-	IPV6 INIT=yes
-	IPV6_AUTOCONF=yes
-	IPV6_DEFROUTE=yes
-	IPV6_FAILURE_FATAL=no
-	NAME=ens160
-	UUID=32d7c5eb-362e-4764-8ddd-847ab5c94e8e
-	DEVICE=ens160
-	ONBOOT=yes
-	IPADDR=$ipAddress123.$ipFourthOctave
-	NETMASK=255.255.255.0
+echo "TYPE=Ethernet
+PROXY_METHOD=none
+BROWSER_ONLY=no
+BOOTPROTO=static
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+IPV6 INIT=yes
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_FAILURE_FATAL=no
+NAME=ens160
+UUID=32d7c5eb-362e-4764-8ddd-847ab5c94e8e
+DEVICE=ens160
+ONBOOT=yes
+IPADDR=$ipAddress123.$ipFourthOctave
+NETMASK=255.255.255.0
 " >> /etc/sysconfig/network-scripts/ifcfg-ens160
 
 systemctl restart NetworkManager
