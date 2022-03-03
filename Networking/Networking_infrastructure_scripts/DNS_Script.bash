8e66ff05c6bc50072c16493c258e1af7d9ca8125
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-03 12:42:12 +0000

    IPV6 changes

--- Networking/Networking_infrastructure_scripts/DNS_Script.bash
+++ Networking/Networking_infrastructure_scripts/DNS_Script.bash
@@ -5,85 +5,107 @@
 # Setting DNS infrastructure
 
 # Argument 1 will be first part of the network address ($ipAddress123)
-ipAddress123="$1"
+ipAddress123="10.0.0"
 
 # Argument 2 will be last part of the network address (3)
 ipFourthOctave="3"
 
 # Argument 4 Domain name  (template)
-domainName="$2"
+domainName="clark"
 
 
 dnf install -y bind bind-utils
 
+rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
 
+echo "TYPE=Ethernet
+PROXY_METHOD=none
+BROWSER_ONLY=no
+BOOTPROTO=static
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+IPV6 INIT=yes
+IPV6_AUTOCONF=yes
+IPV6_DEFROUTE=yes
+IPV6_FAILURE_FATAL=no
+NAME=ens160
+UUID=32d7c5eb-362e-4764-8ddd-847ab5c94e8e
+DEVICE=ens160
+ONBOOT=yes
+IPADDR=$ipAddress123.$ipFourthOctave
+NETMASK=255.255.255.0
+" >> /etc/sysconfig/network-scripts/ifcfg-ens160
 
 # start the DNS server using the command
 systemctl start named
 systemctl enable named
 
-systemctl status named
-
-
+#systemctl status named
 
 rm -f /etc/named.conf
 
-echo "options {
-	listen-on port 53 { $ipAddress123.$ipFourthOctave; }; 
-	listen-on-v6 port 53 { ::1; };
-	directory		"/var/named";
-	dump-file 		"/var/named/data/cache_dump.db";
-	statistics-file "zuar/named/data/named stats.txt";
-	memstatistics-file "/var/named/data/named_mem stats.txt";
-	secroots-file "zuar/named/data/named.secroots";
-	recursing-file "/var/named/data/named .recursing"
-	allow-query (localhost: $ipAddress123.0/24; };
-	
-	recursion yes;
-	dnssec-enable yes;
-	dnssec-validation yes;
-		
-	managed-keys-directory "/var/named/dynamic";
-		
-	pid-file "/run/named/named.pid";
-	session-keyf ile "/run/named/session.key";
-		
-	/* https://fedoraproject.org/wiki/Changes/CryptoPolicy */
-	include "/etc/crypto-policies/back-ends/bind.config"
+echo "//named.conf
+
+options {
+	listen-on port 53 { 10.0.0.3; };
+        listen-on-v6 port 53 { ::1; };
+        directory               "/var/named";
+        dump-file               "/var/named/data/cache_dump.db";
+        statistics-file "var/named/data/named_stats.txt";
+        memstatistics-file "/var/named/data/named_mem stats.txt";
+        secroots-file "var/named/data/named.secroots";
+        recursing-file "/var/named/data/named.recursing";
+        allow-query (localhost: 10.0.0.0/24; };
+
+        recursion yes;
+
+        dnssec-enable yes;
+        dnssec-validation yes;
+
+        managed-keys-directory "/var/named/dynamic";
+
+        pid-file "/run/named/named.pid";
+        session-keyf ile "/run/named/session.key";
+
+        include "/etc/crypto-policies/back-ends/bind.config"
 };
 
 logging {
 	channel default_debug {
-		file "data/named.run";
-		severity dynamic;
-	};
+                file "data/named.run";
+                severity dynamic;
+        };
 };
 
-zone "." IN {
+
+zone . IN {
 	type hint;
-	file "named.ca";
+        file "named.ca";
 };
+
+include "/etc/named.efc1912.zone";
+include "/etc/named.root.key"
 
 //foward zone
 
-	zone "$domainName.local" IN {
-type master;
-	file "$domainName.local.db";
-	allow-update { none; };
-	allow-query { any; };
+zone clark.local IN {
+        type master;
+        file "clark.local.db";
+        allow-update { none; };
+        allow-query { any; };
 };
+
 
 //back zone
 
-	zone "$ipAddress123.192.in-addr.arpa" IN {
-	type master;
-	file "$domainName.local.rev";
-	allow-update { none; };
-	allow-query { any; };
+zone 10.0.0.192.in-addr.arpa IN {
+        type master;
+        file clark.local.rev;
+        allow-update { none; };
+        allow-query { any; };
 };" >> /etc/named.conf
 
 
-rm -f /var/named/$domainName.local.db
 echo "$TTL 86400
 @ IN SOA dns-primary.$domainName.local. admin.$domainName.local. (
 												2020011800 ;Serial
@@ -110,7 +132,7 @@ app  IN   A   $ipAddress123.10
 ftp  IN   CNAME www.$domainName.local." >> /var/named/$domainName.local.db
 
 
-rm -f /var/named/$domainName.local.rev
+
 echo "$TTL 86400
 @ IN SOA dns-primary.$domainName. admin.$domainName.local. (
 											2020011800 ;Serial
@@ -130,8 +152,8 @@ dns-primary     IN      A       $ipAddress123.$ipFourthOctave
 50      IN      PTR     www.$domainName.local." >> /var/named/$domainName.local.rev
 
 
-chown named:named /var/named/$domainName.db
-chown named:named /var/named/$domainName.rev
+chown named:named /var/named/$domainName.local.db
+chown named:named /var/named/$domainName.local.rev
 
 named-checkconf
 named-checkzone $domainName.local /var/named/$domainName.db
@@ -143,25 +165,6 @@ systemctl restart named
 firewall-cmd  --add-service=dns --zone=public  --permanent
 firewall-cmd --reload
 
-rm -f /etc/sysconfig/network-scripts/ifcfg-ens160
-
-echo "TYPE=Ethernet
-PROXY_METHOD=none
-BROWSER_ONLY=no
-BOOTPROTO=static
-DEFROUTE=yes
-IPV4_FAILURE_FATAL=no
-IPV6 INIT=yes
-IPV6_AUTOCONF=yes
-IPV6_DEFROUTE=yes
-IPV6_FAILURE_FATAL=no
-NAME=ens160
-UUID=32d7c5eb-362e-4764-8ddd-847ab5c94e8e
-DEVICE=ens160
-ONBOOT=yes
-IPADDR=$ipAddress123.$ipFourthOctave
-NETMASK=255.255.255.0
-" >> /etc/sysconfig/network-scripts/ifcfg-ens160
 
 systemctl restart NetworkManager
 
