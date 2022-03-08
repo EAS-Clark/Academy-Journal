c86130bdb41ff9d2609bed8107d7f970577805bf
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-08 12:15:32 +0000

    added missing host nodes && edit to etc/sysconfig/dhcpd

--- Networking/Networking_infrastructure_scripts/DHCP_Script.bash
+++ Networking/Networking_infrastructure_scripts/DHCP_Script.bash
@@ -35,29 +35,47 @@ IPADDR=$ipAddress123.2
 NETMASK:255.255.255.0
 GATEWAY=$ipAddress123.1
 DNS=$ipAddress123.3
-DOMAIN=\"$domainName.local\"" >> /etc/sysconfig/network-scripts/ifcfg-ens160
+DOMAIN=$domainName.local" >> /etc/sysconfig/network-scripts/ifcfg-ens160
+
+echo "DHCPDARGS=ens160" >> /etc/sysconfig/dhcpd
+
 
 
 mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.init
 
-echo "default-lease-time	86400; 
-max-lease-time		172800;
+echo "option domain-name "clark.local";
+
+default-lease-time	86400;
+max-lease-time          172800;
 
 ddns-update-style none;
 
 authoritative;
 
-subnet $ipAddress123.0 netmask 255.255.255.0 {
-	range $ipAddress123.10 $ipAddress123.20; 
-	option subnet-mask 255.255.255.0;
-	option routers $ipAddress123.1;
-	option domain-name \"$domainName.local\";
-	option domain-search \"$domainName.local\";
-	option domain-name-servers $ipAddress123.3, 8.8.8.8, 8.8.4.4;
-}" > /etc/dhcp/dhcpd.conf
+subnet 10.0.0.0 netmask 255.255.255.0 {
+        range 10.0.0.10 10.0.0.50;
+        option subnet-mask 255.255.255.0;
+        option routers 10.0.0.1;
+        option domain-name "clark.local";
+        option domain-search "clark.local";
+        option domain-name-servers 10.0.0.3, 8.8.8.8, 8.8.4.4;
+}
+
+
+host gateway-node {
+        hardware ethernet 00:50:56:b2:e8:01;
+        fixed-address 10.0.0.1;
+
+}
+
+host DNS-node {
+        hardware ethernet 00:50:56:b2:23:bd;
+        fixed-address 10.0.0.3;
+
+}" >> /etc/dhcp/dhcpd.conf
 
 firewall-cmd --zone=public --permanent --add-service=dhcp
-firewall-cmd --reload 
+
 
 ifup ens160
 
