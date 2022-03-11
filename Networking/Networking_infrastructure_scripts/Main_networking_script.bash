e6027531ba552e35aa19c42eb8f5e6ec0d02fe98
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-11 11:18:55 +0000

    rejus the lines 3 lines to 1 line

--- Networking/Networking_infrastructure_scripts/Main_networking_script.bash
+++ Networking/Networking_infrastructure_scripts/Main_networking_script.bash
@@ -4,21 +4,14 @@
 ############
 # Setting server infrastructure
 
+scp Gateway_Script.bash DHCP_Script.bash DNS_Script.bash root@192.168.3.107:/bin
 
-scp Gateway_Script.bash root@192.168.3.107:/bin
-scp DHCP_Script.bash root@192.168.3.107:/bin
-scp DNS_Script.bash root@192.168.3.107:/bin
-
-ssh root@192.168.3.107
-cd /bin
-
-chmod 777 Gateway_Script.bash
-chmod 777 DHCP_Script.bash
-chmod 777 DNS_Script.bash
+ssh root@192.168.3.107 "chmod 777 /bin/Gateway_Script.bash /bin/DHCP_Script.bash /bin/DNS_Script.bash"
 
 echo "Running Gateway_Script.bash"
-Gateway_Script.bash
-
+ssh root@192.168.3.107 "Gateway_Script.bash"
+echo "sleeping for 30 seconds" 
+sleep 30s
 
 echo "Running DHCP_Script.bash on 10.0.0.2"
 scp DHCP_Script.bash root@10.0.0.2:/bin
@@ -28,10 +21,9 @@ sleep 30s
 
 echo "Running DNS_Script.bash on 10.0.0.3"
 scp DNS_Script.bash root@10.0.0.3:/bin
-ssh root@10.0.0.2 "/bin/DNS_Script.bash"
-echo "sleeping for 30 seconds"
+ssh root@10.0.0.3 "/bin/DNS_Script.bash"
+echo "sleeping for 30 seconds" 
 sleep 30s
-
 
 
 
