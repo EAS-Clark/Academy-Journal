f5c94d4f1d8ca8552c11ada11a2afc9098a3990e
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-14 08:05:53 +0000

    Fix errors && removed all exit

--- Networking/Networking_infrastructure_scripts/Main_networking_script.bash
+++ Networking/Networking_infrastructure_scripts/Main_networking_script.bash
@@ -8,33 +8,24 @@ scp Gateway_Script.bash DHCP_Script.bash DNS_Script.bash root@192.168.3.107:/bin
 
 ssh root@192.168.3.107 "chmod 777 /bin/Gateway_Script.bash /bin/DHCP_Script.bash /bin/DNS_Script.bash"
 
-scp DHCP_Script.bash root@10.0.0.2:/bin
-scp DNS_Script.bash root@10.0.0.3:/bin
+ssh -t root@192.168.3.107 'scp /bin/DHCP_Script.bash root@10.0.0.2:/bin'
+ssh -t root@192.168.3.107 'scp /bin/DNS_Script.bash root@10.0.0.3:/bin && exit'
 
 
 echo "Running Gateway_Script.bash"
-ssh root@192.168.3.107 "Gateway_Script.bash"
-echo "sleeping for 30 seconds" 
-sleep 30s
-exit
+ssh root@192.168.3.107 "/bin/Gateway_Script.bash && exit"
+
 
 echo "Running DHCP_Script.bash on 10.0.0.2"
-ssh -t root@192.168.3.107 ssh root@10.0.0.2 
-/bin/DHCP_Script.bash
-echo "sleeping for 30 seconds"
-sleep 30s
-exit
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "/bin/DHCP_Script.bash && exit"
+
 
 echo "Running DNS_Script.bash on 10.0.0.3"
-ssh -t root@192.168.3.107 ssh root@10.0.0.3
-/bin/DNS_Script.bash
-echo "sleeping for 30 seconds" 
-sleep 30s
-exit
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "/bin/DNS_Script.bash && exit"
+
 
 
 echo "All serversse have been installed. Enjoy your new netowrk"
-
 
 
 
