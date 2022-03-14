c47ac639da8435e0c06988926581ea321ff6b15d
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-14 07:12:50 +0000

    Remade to use ssh tunnel

--- Networking/Networking_infrastructure_scripts/Main_networking_script.bash
+++ Networking/Networking_infrastructure_scripts/Main_networking_script.bash
@@ -8,24 +8,32 @@ scp Gateway_Script.bash DHCP_Script.bash DNS_Script.bash root@192.168.3.107:/bin
 
 ssh root@192.168.3.107 "chmod 777 /bin/Gateway_Script.bash /bin/DHCP_Script.bash /bin/DNS_Script.bash"
 
+scp DHCP_Script.bash root@10.0.0.2:/bin
+scp DNS_Script.bash root@10.0.0.3:/bin
+
+
 echo "Running Gateway_Script.bash"
 ssh root@192.168.3.107 "Gateway_Script.bash"
 echo "sleeping for 30 seconds" 
 sleep 30s
+exit
 
 echo "Running DHCP_Script.bash on 10.0.0.2"
-scp DHCP_Script.bash root@10.0.0.2:/bin
-ssh root@10.0.0.2 "/bin/DHCP_Script.bash"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 
+/bin/DHCP_Script.bash
 echo "sleeping for 30 seconds"
 sleep 30s
+exit
 
 echo "Running DNS_Script.bash on 10.0.0.3"
-scp DNS_Script.bash root@10.0.0.3:/bin
-ssh root@10.0.0.3 "/bin/DNS_Script.bash"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3
+/bin/DNS_Script.bash
 echo "sleeping for 30 seconds" 
 sleep 30s
+exit
 
 
+echo "All serversse have been installed. Enjoy your new netowrk"
 
 
 
