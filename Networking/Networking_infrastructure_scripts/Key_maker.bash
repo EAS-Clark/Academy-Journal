eb501d9adccd28a4844f7a32cb80a4d473ce5783
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-15 14:25:25 +0000

    Made Key_maker.bash

---
+++ Networking/Networking_infrastructure_scripts/Key_maker.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+#############
+#Clark brooks
+############
+#Makes ssh key and sends them to vms 
+
+ssh-keygen
+ssh-copy-id root@192.168.3.107
+
+#gateway
+ssh root@192.168.3.107 "ssh-keygen"
+ssh -t root@192.168.3.107 "ssh-copy-id root@10.0.0.2"
+ssh -t root@192.168.3.107 "ssh-copy-id root@10.0.0.3"
+ssh -t root@192.168.3.107 "ssh-copy-id root@10.0.0.21"
