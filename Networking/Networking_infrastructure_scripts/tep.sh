b3e490d85083394a19d696a5b0bc9eab1b931d0d
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-15 11:44:32 +0000

    app commands

--- Networking/Networking_infrastructure_scripts/tep.sh
+++ Networking/Networking_infrastructure_scripts/tep.sh
@@ -4,6 +4,12 @@
 #############
 
 git clone https://github.com/Enterprise-Automation/trainee-challenge-node-app.git
+zip -r /trainee-challenge-node-app/
+scp trainee-challenge-node-app.zip root@192.168.3.107:/bin
+
+unzip trainee-challenge-node-app.zip
+PATH = 
+PORT =
 
 
 #my pc 
