66347a7614a9327379260d91e678aad70a2887a3
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-16 08:36:15 +0000

    developed app obtaining and deployment

--- Networking/Networking_infrastructure_scripts/Main_networking_script.bash
+++ Networking/Networking_infrastructure_scripts/Main_networking_script.bash
@@ -25,11 +25,33 @@ ssh -t root@192.168.3.107 ssh root@10.0.0.2 "/bin/DHCP_Script.bash && exit"
 echo "Running DNS_Script.bash on 10.0.0.3"
 ssh -t root@192.168.3.107 ssh root@10.0.0.3 "/bin/DNS_Script.bash && exit"
 
-
-
 echo "All serversse have been installed. Enjoy your new netowrk"
 
 
 
 
+git clone https://github.com/Enterprise-Automation/trainee-challenge-node-app.git
+zip -r /trainee-challenge-node-app/
+
+scp trainee-challenge-node-app.zip root@192.168.3.107:/
+
+ssh -t root@192.168.3.107 'scp /trainee-challenge-node-app.zip root@10.0.0.2:/bin'
+ssh -t root@192.168.3.107 'scp /trainee-challenge-node-app.zip root@10.0.0.3:/bin'
+
+
+ssh root@192.168.3.107 "unzip /trainee-challenge-node-app.zip /var/app/"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "unzip /trainee-challenge-node-app.zip /var/app/"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "unzip /trainee-challenge-node-app.zip /var/app/"
+
+ssh root@192.168.3.107 "TARGET_URL = DHCP && PORT = 80"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "TARGET_URL = DNS && PORT = 80"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "TARGET_URL Gateway = && PORT = 80"
+
+ssh root@192.168.3.107 "firewall-cmd --permanent --add-port=80 && firewall-cmd --reload"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "firewall-cmd --permanent --add-port=80 && firewall-cmd --reload"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "firewall-cmd --permanent --add-port=80 && firewall-cmd --reload"
+
+ssh root@192.168.3.107 "ifdown ens160 && ifup ens160"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "ifdown ens160 && ifup ens160"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "ifdown ens160 && ifup ens160"
 
