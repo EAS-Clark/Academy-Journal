b2a11fa0a382c1617d34c2ace27aa6ab7f6ee0ee
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-16 08:36:09 +0000

    developed app obtaining and deployment

--- Networking/Networking_infrastructure_scripts/tep.sh
+++ Networking/Networking_infrastructure_scripts/tep.sh
@@ -3,19 +3,33 @@
 #Clark brooks
 #############
 
+
+
+
 git clone https://github.com/Enterprise-Automation/trainee-challenge-node-app.git
 zip -r /trainee-challenge-node-app/
-scp trainee-challenge-node-app.zip root@192.168.3.107:/bin
 
-unzip trainee-challenge-node-app.zip
-PATH = 
-PORT =
+scp trainee-challenge-node-app.zip root@192.168.3.107:/
 
-
-#my pc 
+ssh -t root@192.168.3.107 'scp /trainee-challenge-node-app.zip root@10.0.0.2:/bin'
+ssh -t root@192.168.3.107 'scp /trainee-challenge-node-app.zip root@10.0.0.3:/bin'
 
 
+ssh root@192.168.3.107 "unzip /trainee-challenge-node-app.zip /var/app/"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "unzip /trainee-challenge-node-app.zip /var/app/"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "unzip /trainee-challenge-node-app.zip /var/app/"
 
+ssh root@192.168.3.107 "TARGET_URL = DHCP && PORT = 80"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "TARGET_URL = DNS && PORT = 80"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "TARGET_URL Gateway = && PORT = 80"
+
+ssh root@192.168.3.107 "firewall-cmd --permanent --add-port=80 && firewall-cmd --reload"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "firewall-cmd --permanent --add-port=80 && firewall-cmd --reload"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "firewall-cmd --permanent --add-port=80 && firewall-cmd --reload"
+
+ssh root@192.168.3.107 "ifdown ens160 && ifup ens160"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "ifdown ens160 && ifup ens160"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "ifdown ens160 && ifup ens160"
 
 
 
