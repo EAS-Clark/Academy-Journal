2980d5f11b9ff7d382152ab9d6cdab70085dfff2
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-22 13:25:00 +0000

    Added app installer code to the main scrpt from tep.sh

--- Networking/Networking_infrastructure_scripts/Main_networking_script.bash
+++ Networking/Networking_infrastructure_scripts/Main_networking_script.bash
@@ -49,3 +49,28 @@ echo "All serversse have been installed. Enjoy your new network"
 
 echo "Setting up apps now"
 
+#git clone https://github.com/Enterprise-Automation/trainee-challenge-node-app.git
+#zip trainee-challenge-node-app.zip trainee-challenge-node-app 
+
+scp trainee-challenge-node-app.zip root@192.168.3.107:/
+ssh root@192.168.3.107 "chmod 777 /trainee-challenge-node-app.zip"
+
+ssh -t root@192.168.3.107 "scp /trainee-challenge-node-app.zip root@10.0.0.2:/"
+ssh -t root@192.168.3.107 "scp /trainee-challenge-node-app.zip root@10.0.0.3:/"
+
+scp App_installer.bash root@192.168.3.107:/bin
+
+ssh root@192.168.3.107 "chmod 777 /bin/App_installer.bash"
+ssh -t root@192.168.3.107 'scp /bin/App_installer.bash root@10.0.0.2:/bin'
+ssh -t root@192.168.3.107 "scp /bin/App_installer.bash root@10.0.0.3:/bin"
+
+echo "Running on DHCP"
+ssh -t root@192.168.3.107 ssh root@10.0.0.2 "/bin/App_installer.bash DNS 80"
+echo "waiting"
+sleep 30s
+echo "Running on DNS"
+ssh -t root@192.168.3.107 ssh root@10.0.0.3 "/bin/App_installer.bash Gateway 80"
+#echo "waiting"
+#sleep 30s
+#ssh root@192.168.3.107 "/bin/App_installer.bash DHCP 80"
+#echo "waiting running on Gateway"
