121f308f892eb41a22e59bc02e3d03ae3802619b
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-23 16:26:01 +0000

    inquirer test

--- Programming/Java Script/phonebook/Phoneook.js
+++ Programming/Java Script/phonebook/Phoneook.js
@@ -1,32 +1,60 @@
 //node modules 
-const inquirer = reqired('inquirer')
+const inquirer = require("inquirer");
 
-console.log('Hello world');
-
-
-// Constructor funcation
 class Contact {
-    firstName = '';
-    lastName = '';
-    phoneNumber = '';
-
     constructor(firstName, lastName, phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
-
-        this.print = function () {
-            console.log("Name: " + firstName + " " + lastName + "\nPhone number: " + phoneNumber);
-        };
-
     }
 }
 
+let phoneBook = [];
 
+let test1 = new Contact("Clark","Brooks","2324234")
+let test2 = new Contact("Tim","sdfsdf","8765")
+let test3 = new Contact("Max","sfg","467")
 
-const persion = new Contact('clark', 'brooks', '1234234');
-persion.print();
+phoneBook = [test1, test2];
+phoneBook.unshift(test3);
 
+function ContactAdd(){
+    inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "First name?",
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "Last name?",
+        },
+        {
+            name: "phone_number",
+            type: "input",
+            message: "Phone number?",
 
+        }
+    ]).then((answer) => {
+        let tep = new Contact(answer.first_name,answer.last_name,answer.phone_number)
+        phoneBook.unshift(tep);
+    });
+}
 
+function ContactPrintAll(){
+    for(let i =0; i < phoneBook.length; i++){
+        console.log(phoneBook[i].firstName);
+    }
+    
+}
 
+function Run(){
+    
+    ContactAdd();
+    
+ 
+    ContactPrintAll();
+}
+
+Run();
