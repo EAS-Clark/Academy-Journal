0f9e32df1ec21f943d763cd947ad30ae775d813d
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-03-29 15:25:57 +0100

    Making obects from the yaml file

--- Programming/Java Script/kk_challenge 2/ResourceManagement.js
+++ Programming/Java Script/kk_challenge 2/ResourceManagement.js
@@ -1,6 +1,8 @@
 const yaml = require('js-yaml');
+const fs   = require('fs')
 
-class Namepace {
+
+class namespace {
     constructor(total, containers, initContainers, sidecars) {
         this.total = total;
         this.containers = containers;
@@ -10,20 +12,20 @@ class Namepace {
 }
 
 class limit {
-    constructor(cpu, men){
-        this.cpu = cpu;
-        this.men = men;
+    constructor(ob){
+        this.cpu = ob.cpu;
+        this.mem = ob.mem;
     }
 }
 
 class request {
-    constructor(cpu, men){
-        this.cpu = cpu;
-        this.men = men;
+   constructor(ob){
+        this.cpu = ob.cpu;
+        this.mem = ob.mem;
     }
 }
 
-class Resource {
+class resource {
     constructor(limit, request){
         this.limit = limit;
         this.request = request;
@@ -31,22 +33,58 @@ class Resource {
 }
 
 
-function run(){
 
+
+
+function run(){
+/*
     const total = new Resource(new limit(4,3072), new request(2,2048));
     const containers = new Resource( new limit(2,2048), new request(1,1024));
     const initContainers = new Resource( new limit(2,2048), new request(1,1024));
     const sidecars = new Resource( new limit(0.30,1024), new request(0.25,512));
 
-    const namepace = new Namepace(total, containers, initContainers, sidecars);
+    const namespace = new Namepace(total, containers, initContainers, sidecars);
 
-    console.log(namepace)
-    console.log(namepace.initContainers.limit.cpu)
-    console.log(namepace.initContainers.limit.men)
+    console.log(namespace);
+    console.log(namespace.initContainers.limit.cpu);
+    console.log(namespace.initContainers.limit.men);
+*/
+
+
+    try {
+      const doc = yaml.load(fs.readFileSync('limits.yaml', 'utf8'));
+      console.log(doc.total);
+      console.log(Object.keys(doc));
+      console.log(doc.namespace.namespace1);
+  
+      const total = new resource(new limit(doc.namespace.namespace1.total.limit), new request(doc.namespace.namespace1.total.request));
+      const containers = new resource(new limit(doc.namespace.namespace1.containers.limit), new request(doc.namespace.namespace1.containers.request));
+      const initContainers = new resource(new limit(doc.namespace.namespace1.initContainers.limit), new request(doc.namespace.namespace1.initContainers.request));
+      const sidecars = new resource( new limit(doc.namespace.namespace1.sidecars.limit), new request(doc.namespace.namespace1.sidecars.request));
+  
+      const dsfsdf = new namespace(total, containers, initContainers, sidecars);
+  
+      console.log(dsfsdf);
+      console.log(dsfsdf.initContainers.limit.cpu);
+      console.log(dsfsdf.initContainers.limit.men);
+
+
+
+      
+    } catch (e) {
+      console.log(e);
+    }
+    
+    
+
 }
 
 run();
 
+
+// https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
+// https://stackabuse.com/reading-and-writing-yaml-to-a-file-in-node-js-javascript/
+//
 
 /*
 namespace1:
