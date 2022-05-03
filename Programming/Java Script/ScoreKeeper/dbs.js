d90f25f62697e004746dab41c87c4542f129c4ef
EAS-Clark <clark@enterpriseautomation.co.uk>
2022-05-03 15:39:25 +0100

    set up

--- Programming/Java Script/ScoreKeeper/dbs.js
+++ Programming/Java Script/ScoreKeeper/dbs.js
@@ -1,1 +1,18 @@
-const {convertCsv } = require();
+'use strict';
+
+const fs = require("fs");
+let filename = "data/score.json";
+
+function getData(filename){
+    
+    fs.readFile(filename, "utf8", (err, jsonString) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }
+        return jsonString;
+    });
+
+}
+
+console.log( getData("data/score.json"));
